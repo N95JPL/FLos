8931fc5ef36e902863d0042ebee9218c904bb4ce
N95JPL <37276225+N95JPL@users.noreply.github.com>
2022-11-24 21:59:54 +0000

    Include Build Files

---
+++ build/resources/VariableMaps/MediumSpeedVar.js
@@ -0,0 +1,76 @@
+/* eslint-disable prefer-const */
+let mediumSpeed = {
+  time: {
+    hour: 0,
+    minute: 0,
+    second: 0,
+  },
+  temperature: {
+    driver: "--",
+    passenger: "--",
+    interior: "--",
+  },
+  indicators: {
+    rearHeater: 0,
+    auto: 0,
+    defrost: 0,
+    frontHeater: 0,
+    recirc: 0,
+  },
+  brightness: {
+    adjustedLight: 0,
+    offset: 20,
+    auto: true,
+  },
+  vehicleSettings: {
+    Drive_Away_Locking_5: false,
+    Drive_Away_Locking_10: false,
+    Drive_Away_Locking_15: false,
+    Passive_Arming: false,
+    AutoLock: false,
+    Alarm_Sensors: false,
+    Two_Stage_Locking: false,
+    Global_Windows_Open: false,
+    Global_Windows_Close: false,
+    Mirror_Fold: false,
+  },
+};
+exports.mediumSpeed = mediumSpeed;
+
+let mediumSpeedPrev = {
+  time: {
+    hour: 0,
+    minute: 0,
+    second: 0,
+  },
+  temperature: {
+    driver: "--",
+    passenger: "--",
+    interior: "--",
+  },
+  indicators: {
+    rearHeater: 0,
+    auto: 0,
+    defrost: 0,
+    frontHeater: 0,
+    recirc: 0,
+  },
+  brightness: {
+    adjustedLight: 0,
+    offset: 20,
+    auto: true,
+  },
+  vehicleSettings: {
+    Drive_Away_Locking_5: false,
+    Drive_Away_Locking_10: false,
+    Drive_Away_Locking_15: false,
+    Passive_Arming: false,
+    AutoLock: false,
+    Alarm_Sensors: false,
+    Two_Stage_Locking: false,
+    Global_Windows_Open: false,
+    Global_Windows_Close: false,
+    Mirror_Fold: false,
+  },
+};
+exports.mediumSpeedPrev = mediumSpeedPrev;
