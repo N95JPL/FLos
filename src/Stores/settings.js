3504245789ad22672e5dab7b025acfee252bd034
N95JPL <37276225+N95JPL@users.noreply.github.com>
2022-11-24 21:30:53 +0000

    New images - Persist settings

--- src/Stores/settings.js
+++ src/Stores/settings.js
@@ -1,8 +1,16 @@
 import create from "zustand";
+import { persist } from 'zustand/middleware'
 
-export let settings = create((set) => ({
-  brightnessOffset: 25,
-  setBrightnessOffset: (brightnessOffset) => set({ brightnessOffset }),
-  brightnessAuto: true,
-  setBrightnessAuto: (brightnessAuto) => set({ brightnessAuto }),
-}));
+export let settings = create(
+  persist(
+    (set) => ({
+      brightnessOffset: 25,
+      setBrightnessOffset: (brightnessOffset) => set({ brightnessOffset }),
+      brightnessAuto: true,
+      setBrightnessAuto: (brightnessAuto) => set({ brightnessAuto }),
+    }),
+    {
+      name: 'settings'
+    }
+  )
+);
--- src/Stores/theme.js
+++ src/Stores/theme.js
@@ -1,10 +1,17 @@
 import create from "zustand";
-
-export let theme = create((set) => ({
-  darkMode: false,
-  setDarkMode: (darkMode) => set({ darkMode }),
-  primaryColor: "sky",
-  setPrimaryColor: (primaryColor) => set({ primaryColor }),
-  secondaryColor: "pink",
-  setSecondaryColor: (secondaryColor) => set({ secondaryColor }),
-}));
+import { persist } from 'zustand/middleware';
+export let theme = create(
+  persist(
+    (set) => ({
+      darkMode: false,
+      setDarkMode: (darkMode) => set({ darkMode }),
+      primaryColor: "sky",
+      setPrimaryColor: (primaryColor) => set({ primaryColor }),
+      secondaryColor: "pink",
+      setSecondaryColor: (secondaryColor) => set({ secondaryColor }),
+    }),
+    {
+      name: 'theme'
+    }
+  )
+);
