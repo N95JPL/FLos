59924198d45ec538f5932562e6078e1da5ce4188
N95JPL <37276225+N95JPL@users.noreply.github.com>
2022-11-30 11:22:39 +0000

    Add Go-Back | Correct Server File

--- build/server.js
+++ build/server.js
@@ -8,6 +8,7 @@ const { Out } = require("./resources/CanMap/canOut");
 const { SettingsOut } = require("./resources/CanMap/canSetting");
 const can = require("socketcan");
 const fs = require("fs");
+const { log } = require("./electron")
 let changedMedium = {
   time: {},
   temperature: {},
--- src/Modules/Carplay.js
+++ src/Modules/Carplay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./Style.css";
 
@@ -6,15 +7,9 @@ function Carplay() {
   return (
     <div className="h-screen flex items-center justify-center p-10">
       <div className="max-w-4xl w-full flex flex-col">
-        <div>
-          <div>
-            <h1 className="text-2xl font-extrabold tracking-tight">CarPlay!</h1>
-            <p className="text-sm mb-2.5">Lol, this will get done soon!</p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-2.5 mt-2.5">
-          <div className="flex gap-2.5"></div>
-        </div>
+        <h1 className="text-2xl font-extrabold tracking-tight">CarPlay!</h1>
+        <p className="text-sm mb-2.5">Lol, this will get done soon!</p>
+        <Link to="/"><button className="bg-gray-800 text-white px-4 py-2 rounded-md">Go Back</button></Link>
       </div>
     </div>
   );
