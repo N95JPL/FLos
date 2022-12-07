c502f07faae374e9b33e7b7ff553e771274481b5
N95JPL <37276225+N95JPL@users.noreply.github.com>
2022-12-07 20:21:03 +0000

    Upload all models CCF

--- Tools/XMLConvert.js
+++ Tools/XMLConvert.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 const files = fs
-  .readdirSync("./public/resources/XML/")
-  .filter((fn) => fn.includes(".xml"));
+  .readdirSync("../Tools/temp")
+  .filter((fn) => fn.includes(".exml-decrypted"));
 const xmlParser = require("xml2json");
 
 for (const a in files) {
   console.log("Processing: " + files[a] + " | " + a + "/" + files.length);
-  var data = fs.readFileSync("./public/resources/XML/CCF-XML" + files[a]);
+  var data = fs.readFileSync("../Tools/temp/" + files[a]);
   const xmlObj = xmlParser.toJson(data, {
     reversible: true,
     coerce: true,
     alternateTextNode: "value",
   });
   fs.writeFileSync(
-    "./public/resources/XML/CCF/" + files[a].replace(".xml", "") + ".json",
+    "../public/resources/XML/CCF-XML/" + files[a].replace(".exml-decrypted", "") + ".json",
     xmlObj
   );
 }
